13d85363ee0f9a91ed45445eb284a528eef95f40
Jens <112494714+Taggen14@users.noreply.github.com>
2026-03-29 21:26:11 +0200

    added migration in build script, and added new migration file

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260329_192333 from './20260329_192333';
+
+export const migrations = [
+  {
+    up: migration_20260329_192333.up,
+    down: migration_20260329_192333.down,
+    name: '20260329_192333'
+  },
+];
